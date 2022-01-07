8f619beb76dfca509e6b520e44403e957f749cdd
K-Shikhar <79716970+K-Shikhar@users.noreply.github.com>
2022-01-07 16:56:50 +0530

    Add files via upload

---
+++ Amazon/Solution6.cpp
@@ -0,0 +1,33 @@
+vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        vector<int> ans;
+        
+        deque<int> dq;
+        int i=0,j=0;
+        while(j<n)
+        {
+            while(!dq.empty() && dq.back()<arr[j])
+            {
+                dq.pop_back();
+            }
+            
+            dq.push_back(arr[j]);
+            
+            if(j-i+1<k)
+            {
+                j++;
+            }
+            else
+            {
+                ans.push_back(dq.front());
+                
+                if(arr[i]==dq.front())
+                {
+                    dq.pop_front();
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
