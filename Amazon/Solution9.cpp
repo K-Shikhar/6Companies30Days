7118c9e23c7e7146dae5ef2d3767a8af98cd85d4
K-Shikhar <79716970+K-Shikhar@users.noreply.github.com>
2022-01-11 22:08:41 +0530

    Update Solution9.cpp

--- Amazon/Solution9.cpp
+++ Amazon/Solution9.cpp
@@ -1,20 +1,23 @@
 class Solution{
 public:
     int isValid(vector<vector<int>> mat){
-        bool row[10][10] = {false},col[10][10] = {false},grid[10][10] = {false};
+        int row[9][9]={0};
+        int col[9][9]={0};
+        int k[9][9]={0};
         
         for(int i = 0; i < 9; i++) 
         {
             for(int j = 0; j < 9; j++) 
             {
-                int num = mat[i][j];
-                if(num == 0) continue;
+                if(mat[i][j] == 0) continue;
                 int in = (i/3) * 3 + (j/3);
-                if(row[i][num] || col[j][num] || grid[in][num])
+                if(row[i][mat[i][j]-1] || col[j][mat[i][j]-1] || k[in][mat[i][j]-1])
+                {
                     return 0;
-                row[i][num] = true;
-                 col[j][num] = true;
-                  grid[in][num] = true;
+                }
+                row[i][mat[i][j]-1] = 1;
+                col[j][mat[i][j]-1] = 1;
+                k[in][mat[i][j]-1] = 1;
             }
         }
         return 1;
