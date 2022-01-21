d14a72c921a620519f6f4332a99c1af79b9d2d00
K-Shikhar <79716970+K-Shikhar@users.noreply.github.com>
2022-01-21 13:58:09 +0530

    Update AdobeSol2.cpp

--- Adobe/AdobeSol2.cpp
+++ Adobe/AdobeSol2.cpp
@@ -1,33 +1,20 @@
-int lengthOfLongestAP(int arr[], int n) {
-        if(n==1)
-        {
-            return 1;
-        }
-        if(n==2)
-        {
-            return 2;
-        }
-        map<int,int> m;
-        for(int i=0;i<n;i++)
-        {
-            m[arr[i]]++;
-        }
-        int maxm=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            
-            for(int j=i+1;j<n;j++)
-            {
-                int len=1;
-                int diff=arr[j]-arr[i];
-                int k=arr[j];
-                while(m.find(k)!=m.end() && k<=arr[n-1])
-                {
-                    k+=diff;
-                    len++;
-                }
-                maxm=max(maxm,len);
-            }
-        }
-        return maxm;
+    typedef long long ll;
+    const ll mod = 1000000007;
+    ll recur(int N,int k,int prev_cons,int idx,vector<vector<ll>> &dp)
+    {
+        if(idx == N) return 1;
+        
+        if(dp[idx][prev_cons] != -1)
+            return  dp[idx][prev_cons];
+        
+        if(prev_cons >= k)
+            return dp[idx][prev_cons] = (21%mod * recur(N,k,0,idx+1,dp)%mod)%mod;
+        else
+            return dp[idx][prev_cons] = ((5%mod * recur(N,k,prev_cons+1,idx+1,dp))%mod + (21%mod * recur(N,k,0,idx+1,dp)%mod)%mod)%mod;
+    }
+    
+    int kvowelwords(int N, int K) 
+    {
+        vector<vector<ll>> dp(N+1,vector<ll>(K+1,-1));
+        return recur(N,K,0,0,dp)%mod;
     }
