6b27aacc80d53a2ee34b749ad4470058b878b4e9
K-Shikhar <79716970+K-Shikhar@users.noreply.github.com>
2022-01-21 14:08:44 +0530

    Update AdobeSol3.cpp

--- Adobe/AdobeSol3.cpp
+++ Adobe/AdobeSol3.cpp
@@ -1,24 +1,22 @@
-int kvowelwords(int N, int K) 
+ typedef long long ll;
+    const ll mod = 1000000007;
+    
+    
+    ll recur(int N,int k,int prev_cons,int idx,vector<vector<ll>> &dp)
     {
-        long long dp[N+1][K+1];
-        long long mod=1000000007;
-        for(int i=0;i<=N;i++)
-        {
-            for(int j=0;j<=K;j++)
-            {
-                if(i==0)
-                {
-                    dp[i][j]=1;   
-                }
-                else
-                {
-                    dp[i][j]=dp[i-1][K]*21%mod;
-                    if(j>0)
-                    {
-                        dp[i][j]=(dp[i][j]+dp[i-1][j-1]*5%mod)%mod;
-                    }
-                }
-            }
-        }
-        return dp[N][K];
+        if(idx == N) return 1;
+        
+        if(dp[idx][prev_cons] != -1)
+            return  dp[idx][prev_cons];
+        
+        if(prev_cons >= k)
+            return dp[idx][prev_cons] = (21%mod * recur(N,k,0,idx+1,dp)%mod)%mod;
+        else
+            return dp[idx][prev_cons] = ((5%mod * recur(N,k,prev_cons+1,idx+1,dp))%mod + (21%mod * recur(N,k,0,idx+1,dp)%mod)%mod)%mod;
+    }
+    
+    int kvowelwords(int N, int K) 
+    {
+        vector<vector<ll>> dp(N+1,vector<ll>(K+1,-1));
+        return recur(N,K,0,0,dp)%mod;
     }
